3540820ac254fb455206dcf9653aa477500bccd6
MZD <michael@caracolazul.dev>
2024-06-05 22:54:52 -0400

    update pseudocode for install.sh

--- install.sh
+++ install.sh
@@ -18,13 +18,21 @@
 # auto-detect installation of dir or file
 # use use soft-links unless is-file && user-confirmed
 
-# install .profile
+# install .rcd
 
 # install .bashrc or .zshrc
 
-# install .ssh configs
+# install .ssh/config
 
 # install .gitconfig
+
+# Final install should look like:
+# ll | grep dot-profile
+# drwxrwxr-x  4 mzd  mzd  4.0K Jun  5 22:14 .dot-profile/
+# lrwxrwxrwx  1 mzd  mzd    17 Jun  5 22:11 .rcd -> .dot-profile/.rcd/
+# lrwxrwxrwx  1 mzd  mzd    20 Jun  5 22:10 .bashrc -> .dot-profile/.bashrc
+# lrwxrwxrwx  1 mzd  mzd    27 Jun  5 20:24 .gitconfig -> .dot-profile/.gitconfig-mzd
+# lrwxrwxrwx  1 mzd  mzd    24 Jun  5 21:56 .ssh_config -> .dot-profile/.ssh_config
 
 
 
